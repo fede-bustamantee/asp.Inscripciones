28bd157eb6baf45935a078c63db8b13b0b32b62a
fede <federicobustamantevyp@gmail.com>
2024-05-27 17:45:27 -0300

    arreglando el gitignore

---
+++ Models/InscripcionesContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Inscripciones.Models
+{
+    public class InscripcionesContext : DbContext
+    {
+        public InscripcionesContext(DbContextOptions<InscripcionesContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=.\SQLEXPRESS;
+                Database=InscripcionesContext; 
+                User Id=sa; Password=1234; 
+                MultipleActiveResultSets = True; Encrypt=False");
+        }
+        public virtual DbSet<Alumno> alumnos { get; set; }
+        public virtual DbSet<Carrera> carreras { get; set; }
+
+
+        //datos semilla
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Alumno>().HasData(
+               new Alumno()
+               {
+                   Id = 1,
+                   ApellidoNombre = "Federico Bustamante",
+                   Telefono = "3498498494",
+                   Direccion = "San Juan 2043",
+                   Email= "federicobustamante@gmail.com"
+               });
+        }
+    }
+}
