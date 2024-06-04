7a17c8463d544c380bcc06081e46883f5051332c
fede <federicobustamantevyp@gmail.com>
2024-06-04 16:52:20 -0300

    FuncionandoConLaIncorporaciondeInscripciones

---
+++ Models/Inscripcion.cs
@@ -0,0 +1,14 @@
+﻿using NuGet.Protocol.Core.Types;
+
+namespace Inscripciones.Models
+{
+    public class Inscripcion
+    {
+        public int Id { get; set; }
+        public DateTime Fecha { get; set; }
+        public int AlumnoId { get; set; }
+        public Alumno? Alumno { get; set; }
+        public int CarreraId { get; set; }
+        public Carrera? Carrera { get; set; }
+    }
+}
